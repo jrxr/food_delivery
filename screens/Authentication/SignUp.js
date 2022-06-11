bf8bfcbf0b72b668a1e16ba09c814dd071fc6fe4
Agacy junior <juniormartinssom@gmail.com>
2022-06-11 14:47:02 -0300

    SignUp finish

--- screens/Authentication/SignUp.js
+++ screens/Authentication/SignUp.js
@@ -1,15 +1,246 @@
-import React from 'react';
-import {
-    View,
-    Text
-} from 'react-native';
+import React from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
 
-const SignUp = () => {
+import { AuthLayout } from "../";
+import { FONTS, SIZES, COLORS, icons } from "../../constants";
+import {
+  FormInput,
+  TextButtonBoarding,
+  TextIconButtonSignIn,
+} from "../../components";
+import { utils } from "../../utils";
+
+const SignUp = ({ navigation }) => {
+  const [email, setEmail] = React.useState("");
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [showPass, setShowPass] = React.useState(false);
+
+  const [emailError, setEmailError] = React.useState("");
+  const [usernameError, setUsernameError] = React.useState("");
+  const [passwordError, setPasswordError] = React.useState("");
+
+  function isEnableSignUp() {
     return (
-        <View>
-            <Text>SignUp</Text>
+      email != "" &&
+      username != "" &&
+      password != "" &&
+      emailError == "" &&
+      usernameError == "" &&
+      passwordError == ""
+    );
+  }
+
+  return (
+    <AuthLayout
+      title="Getting Started"
+      subtitle="Create an account to continue!"
+      titleContainerStyle={{
+        marginTop: SIZES.radius,
+      }}
+    >
+      {/* Form Input and Sign Up */}
+      <View
+        style={{
+          flex: 1,
+          marginTop: SIZES.padding,
+        }}
+      >
+        <FormInput
+          label="Email"
+          keyboardType="email-address"
+          autoCompleteType="email"
+          onChange={(value) => {
+            utils.validateEmail(value, setEmailError);
+
+            setEmail(value);
+          }}
+          errorMsg={emailError}
+          appendComponent={
+            <View
+              style={{
+                justifyContent: "center",
+              }}
+            >
+              <Image
+                source={
+                  email == "" || (email != "" && emailError == "")
+                    ? icons.correct
+                    : icons.cancel
+                }
+                style={{
+                  height: 20,
+                  width: 20,
+                  tintColor:
+                    email == ""
+                      ? COLORS.gray
+                      : email != "" && emailError == ""
+                      ? COLORS.green
+                      : COLORS.red,
+                }}
+              />
+            </View>
+          }
+        />
+        <FormInput
+          label="Username"
+          containerStyle={{
+            marginTop: SIZES.radius,
+          }}
+          onChange={(value) => {
+            setUsername(value);
+          }}
+          errorMsg={usernameError}
+          appendComponent={
+            <View
+              style={{
+                justifyContent: "center",
+              }}
+            >
+              <Image
+                source={
+                  username == "" || (username != "" && usernameError == "")
+                    ? icons.correct
+                    : icons.cancel
+                }
+                style={{
+                  height: 20,
+                  width: 20,
+                  tintColor:
+                    username == ""
+                      ? COLORS.gray
+                      : username != "" && usernameError == ""
+                      ? COLORS.green
+                      : COLORS.red,
+                }}
+              />
+            </View>
+          }
+        />
+        <FormInput
+          label="Password"
+          secureTextEntry={!showPass}
+          autoCompleteType="password"
+          containerStyle={{
+            marginTop: SIZES.radius,
+          }}
+          onChange={(value) => {
+            utils.validatePassword(value, setPasswordError);
+            setPassword(value);
+          }}
+          errorMsg={passwordError}
+          appendComponent={
+            <TouchableOpacity
+              style={{
+                justifyContent: "center",
+                alignItems: "flex-end",
+                width: 40,
+              }}
+              onPress={() => setShowPass(!showPass)}
+            >
+              <Image
+                source={showPass ? icons.eye_close : icons.eye}
+                style={{
+                  height: 20,
+                  width: 20,
+                  tintColor: COLORS.gray,
+                }}
+              />
+            </TouchableOpacity>
+          }
+        />
+
+        {/* Sign Up & Sign In */}
+        <TextButtonBoarding
+          label="Sign Up"
+          disabled={isEnableSignUp() ? false : true}
+          buttonContainerStyle={{
+            height: 55,
+            alignItems: "center",
+            marginTop: SIZES.padding,
+            borderRadius: SIZES.radius,
+            backgroundColor: isEnableSignUp()
+              ? COLORS.primary
+              : COLORS.transparentPrimray,
+          }}
+          onPress={() => navigation.navigate("Otp")}
+        />
+
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            marginTop: SIZES.radius,
+          }}
+        >
+          <Text
+            style={{
+              color: COLORS.darkGray,
+              ...FONTS.body3,
+            }}
+          >
+            Already have an account?
+          </Text>
+          <TextButtonBoarding
+            label=" Sign In"
+            buttonContainerStyle={{
+              backgroundColor: null,
+            }}
+            labelStyle={{
+              color: COLORS.primary,
+              ...FONTS.h3,
+            }}
+            onPress={() => navigation.goBack()}
+          />
         </View>
-    )
-}
+      </View>
+
+      {/* Footer */}
+      <View>
+        {/* Facebook */}
+        <TextIconButtonSignIn
+          containerStyle={{
+            height: 50,
+            alignItems: "center",
+            borderRadius: SIZES.radius,
+            backgroundColor: COLORS.blue,
+          }}
+          icon={icons.fb}
+          iconPosition="LEFT"
+          iconStyle={{
+            tintColor: COLORS.white,
+          }}
+          label="Sign In with Facebook"
+          labelStyle={{
+            marginLeft: SIZES.radius,
+            color: COLORS.white,
+          }}
+          onPress={() => console.log("Facebook")}
+        />
+
+        {/* Google */}
+        <TextIconButtonSignIn
+          containerStyle={{
+            height: 50,
+            alignItems: "center",
+            borderRadius: SIZES.radius,
+            backgroundColor: COLORS.lightGray2,
+            marginTop: SIZES.radius,
+          }}
+          icon={icons.google}
+          iconPosition="RIGHT"
+          iconStyle={{
+            tintColor: null,
+          }}
+          label="Sign In with Google"
+          labelStyle={{
+            marginLeft: SIZES.radius,
+          }}
+          onPress={() => console.log("Google")}
+        />
+      </View>
+    </AuthLayout>
+  );
+};
 
 export default SignUp;
